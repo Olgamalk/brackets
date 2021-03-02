812d0f41d9dfe647c132b1a60d855b1af3d90643
Olga Malkovich <43276405+Olgamalk@users.noreply.github.com>
2021-03-02 14:57:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let map = {
+    ")": "(",
+    "}": "{",
+    "]": "["
+  }
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "(" || str[i] === "[" || str[i] === "{") {
+      stack.push (str[i]);
+    } else if (stack[stack.length - 1] === map[str[i]]) {
+      stack.pop();
+    } else return false;
+  }
+  return stack.length ? false : true;
 }
+
+
